c203800cfee22c7f11f701cf100194d588941b1f
Thomas Krueger <thomas.krueger.home@gmail.com>
2017-08-03 01:03:02 +0200

    development commit with a lot of bug fixes

---
+++ bin/lan_monitor_scan.sh
@@ -0,0 +1,8 @@
+#the folder where the webserver expects the scan report
+HTML_FOLDER=/var/www/lan-monitor
+ 
+#TODO get the IP range to scan from the conf file
+
+nmap -p 22,80 -oX $HTML_FOLDER/last_scan.xml 192.168.0.0/24
+cp  $HTML_FOLDER/last_scan.xml  $HTML_FOLDER/scan.xml
+rm $HTML_FOLDER/last_scan.xml
