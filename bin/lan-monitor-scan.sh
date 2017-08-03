fe255d070ad974d763984ba24c776100ed07f2c0
KruDex <thomas.krueger.home@gmail.com>
2017-08-03 10:55:13 +0200

    renamed the scan script file

--- bin/lan-monitor-scan.sh
+++ bin/lan-monitor-scan.sh
@@ -3,6 +3,6 @@ HTML_FOLDER=/var/www/lan-monitor
  
 #TODO get the IP range to scan from the conf file
 
-nmap -p 22,80 -oX $HTML_FOLDER/last_scan.xml 192.168.0.0/24
+nmap -p 22,80 -oX $HTML_FOLDER/last_scan.xml 192.168.1.0/24
 cp  $HTML_FOLDER/last_scan.xml  $HTML_FOLDER/scan.xml
 rm $HTML_FOLDER/last_scan.xml
