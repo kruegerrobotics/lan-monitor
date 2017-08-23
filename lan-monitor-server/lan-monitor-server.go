1f025c9f890589a701a843fa8c934bc62b63a0f4
Thomas Krueger <thomas.krueger.home@gmail.com>
2017-08-23 22:20:18 +0200

    added simple go webserver and button for defaults without function is the btn

---
+++ lan-monitor-server/lan-monitor-server.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+)
+
+//VERSION of the program
+var VERSION = "1.5.0"
+
+func callNMAP() {
+	for {
+		log.Println("Scanning")
+		cmd := exec.Command("nmap", "-p", "22,80", "-oX", "test.xml")
+		cmd.Stdin = strings.NewReader("some input")
+		var out bytes.Buffer
+		cmd.Stdout = &out
+		err := cmd.Run()
+		if err != nil {
+			log.Println(err)
+		}
+		log.Printf("in all caps: %q\n", out.String())
+		break
+	}
+}
+
+func pageHandler(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path[1:]
+	log.Println("URL path: " + path)
+
+	f, err := os.Open(path)
+	if err == nil {
+		Reader := bufio.NewReader(f)
+
+		var contentType string
+
+		if strings.HasSuffix(path, "css") {
+			contentType = "text/css"
+		} else if strings.HasSuffix(path, ".html") {
+			contentType = "text/html"
+		} else if strings.HasSuffix(path, ".js") {
+			contentType = "application/javascript"
+		} else if strings.HasSuffix(path, ".png") {
+			contentType = "image/png"
+		} else if strings.HasSuffix(path, ".svg") {
+			contentType = "image/svg+xml"
+		} else {
+			contentType = "text/plain"
+		}
+		w.Header().Add("Content Type", contentType)
+		Reader.WriteTo(w)
+	} else {
+		w.WriteHeader(404)
+		fmt.Fprintln(w, "404 - Page not found"+http.StatusText(404))
+	}
+}
+
+func main() {
+	log.Println("Starting lan-monitor-server")
+	displayVersion := flag.Bool("version", false, "Prints the version number")
+	httpPort := flag.Int("port", 8080, "HTTP port for the webserver (some ports e.g. 80 require su permissions)")
+	flag.Parse()
+
+	if *displayVersion == true {
+		fmt.Println("Version: " + VERSION)
+		return
+	}
+
+	//changing working dir
+	log.Println("Changing working dir to: ")
+	err := os.Chdir("../")
+	if err != nil {
+		log.Fatalln("Unable to switch to right dir")
+	}
+	workingDir, _ := os.Getwd()
+	log.Println("Dir:" + workingDir)
+
+	//init the scanning routine
+	go callNMAP()
+
+	//starting the webserver
+	http.HandleFunc("/", pageHandler)
+	err = http.ListenAndServe(":"+strconv.Itoa(*httpPort), nil)
+	if err != nil {
+		log.Println("Server error - " + err.Error())
+	}
+}
