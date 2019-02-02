070012048b2de7fbd76588713410492df39f6523
ohad <ohad111@gmail.com>
2019-02-02 19:55:23 +0200

    created gulp configuration file, linked js and css in HTML.
    Gulp configuration
    =======================
    Gulp will now copy any new file added, track changes in exciting files
    and unlink removed files.
    
    Js files are minified and Scss files are converted to css before copied
    to the `WWW` folder.
    
    Default action were set to run a browserSync server with live reload
    and watch source folder for changes.
    
    To run use `gulp` in the main project folder.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,9 +1,75 @@
-
 const gulp           = require('gulp'),
-      browserSync    = require('browser-sync').create(),
+      browsersync    = require('browser-sync').create(),
       sass           = require('gulp-sass'),
       rename         = require("gulp-rename"),
       uglify         = require('gulp-uglify'),
-      fs             = require('fs');
+      fs             = require('fs'),
+      dir            = {src: 'ui-generator/', dest: 'www/'};
 
-sass.compiler = require('node-sass');
+// Settings for live reload
+let browserSync = cb => {
+    browsersync.init({
+        server: {
+            baseDir: "./www/"
+        },
+        port: 3001
+    });
+    cb();
+};
+
+// Task for sass prepossessing
+let css = () => {
+    return gulp
+        .src(dir.src + 'sass/importer.scss')
+        .pipe(sass({ outputStyle: "expanded" }))
+        .pipe(rename('main.css'))
+        .pipe(gulp.dest(dir.dest + "css"))
+        .pipe(browsersync.stream());
+};
+
+// Task for javascript minification
+let js = () => {
+    return gulp
+        .src(dir.src + 'js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest(dir.dest + "js"))
+        .pipe(browsersync.stream());
+};
+
+// Copy file to destination folder
+let copyFile = pathSrc => {
+    fs.copyFile(pathSrc, dir.dest + pathSrc.substring(pathSrc.indexOf("/") + 1), err => {
+        if (err) throw err;
+        browsersync.reload();
+    })
+};
+
+// Remove deleted file from destination folder
+let unlinkFile = pathSrc => {
+    fs.unlink(dir.dest + pathSrc.substring(pathSrc.indexOf("/") + 1), (err) => {
+        if (err) throw err;
+        browsersync.reload();
+    });
+};
+
+// Method dispatch mapping
+let dispatch = {
+    scss: gulp.parallel(css),
+    js: gulp.parallel(js)
+};
+
+// Watch task
+let watch = cb => {
+    gulp.watch(dir.src + '**/*.*').on('change', path => {
+        let fileExt = /(?:\.([^.]+))?$/.exec(path);
+        dispatch.hasOwnProperty(fileExt[1]) ? dispatch[fileExt[1]]() : copyFile(path);
+    });
+
+    gulp.watch(dir.src + 'img/*.*').on('add', path => copyFile(path));
+    gulp.watch(dir.src + 'img/*.*').on('unlink', path => unlinkFile(path));
+
+    cb();
+};
+
+
+exports.default = gulp.series(browserSync, watch);
