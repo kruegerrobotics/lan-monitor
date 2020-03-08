68164096139ce05751503813bbb7f7fe4c156a62
tkrueger <tkrueger@think-deb.tk.home>
2020-03-08 12:54:08 +0100

    dev commit

--- ui-generator/js/main.js
+++ ui-generator/js/main.js
@@ -1,14 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-// Create WebSocket connection.
-    const socket = new WebSocket('ws://' + location.host + '/ws');
+    // Create WebSocket connection.
+    //let wsurl = location.host
+    let wsurl = "127.0.0.1:8080"
+    const socket = new WebSocket('ws://' + wsurl + '/ws');
 
-// Connection opened
+    // Connection opened
     socket.addEventListener('open', event => {
         socket.send(JSON.stringify({ Commmand: 'request' }));
         console.log('sent');
     });
 
-// Listen for messages
+    // Listen for messages
     socket.addEventListener('message', event => {
         console.log('got msg');
         parseScanResults(JSON.parse(event.data));
@@ -18,11 +20,11 @@ document.addEventListener("DOMContentLoaded", () => {
 // Create an object with the Ip addresses as keys
 let parseScanResults = data => {
     let scanResults = {
-        scanArg : 'Scan arg: ' + data.nmaprun['-args'] + ' ' + data.nmaprun['-startstr'],
-        hosts : data.nmaprun.host.reduce((obj, item) => {
+        scanArg: 'Scan arg: ' + data.nmaprun['-args'] + ' ' + data.nmaprun['-startstr'],
+        hosts: data.nmaprun.host.reduce((obj, item) => {
             obj[item.address['-addr']] = item;
             return obj;
-        } ,{})
+        }, {})
     };
     trackResults(scanResults);
 };
@@ -42,14 +44,23 @@ let trackResults = data => {
 
     for (key of Object.keys(localResults.hosts)) {
         if (newResults.hosts[key] === undefined) localResults.hosts[key].status['-state'] = 'down';
+        console.log(key)
+        let nameNode = document.createElement("li")
+        let nameTextNode = document.createTextNode(key)
+        
+        
+        nameNode.appendChild(nameTextNode)
+        //document.getElementById("scan-results").appendChild(nameNode)
     }
+
     for (key of Object.keys(newResults.hosts)) {
         if (localResults.hosts[key] === undefined || localResults.hosts[key].status['-state'] === 'down') {
             localResults.hosts[key] = newResults.hosts[key];
         }
     }
     localStorage.setItem('scanResults', JSON.stringify(localResults));
-    // update view
+    // update view   
+
 };
 
 
