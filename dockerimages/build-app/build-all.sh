1ff0b0322ac1ab11b76e6afdfc40b5d8666dc524
tkrueger <tkrueger@think-deb.tk.home>
2020-03-23 17:23:58 +0100

    docker build process

--- dockerimages/build-app/build-all.sh
+++ dockerimages/build-app/build-all.sh
@@ -5,7 +5,13 @@ set -e
 echo "Cloning"
 git clone https://github.com/kruegerrobotics/lan-monitor.git
 
-echo "building"
+echo "building the web angular part"
+cd
+cd lan-monitor/webui-ang
+npm install
+
+echo "building the go part"
+cd
 cd lan-monitor/pkg-build
 make -f build-deb-pkg.makefile
 
