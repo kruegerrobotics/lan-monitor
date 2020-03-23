b55fdbe1814582a117da14dd8da278569743a96b
tkrueger <tkrueger@think-deb.tk.home>
2020-03-23 18:29:18 +0100

    docker autobuild container with angular

--- dockerimages/build-app/build-all.sh
+++ dockerimages/build-app/build-all.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
+
 # exit when any command fails
 set -e
 
 echo "Cloning"
 git clone https://github.com/kruegerrobotics/lan-monitor.git
 
+echo "changing branch"
+cd lan-monitor
+git checkout ux-upgrade
+
 echo "building the web angular part"
-cd
-cd lan-monitor/webui-ang
+cd webui-ang
 npm install
 
 echo "building the go part"
-cd
-cd lan-monitor/pkg-build
+cd ..
+cd pkg-build
 make -f build-deb-pkg.makefile
 
 
