632aeb0c8d99aa44015715b6ec80c7af924b0d4e
tkrueger <tkrueger@think-deb.tk.home>
2020-03-24 20:58:31 +0100

    docker build file refering to right branch

--- dockerimages/build-app/build-all.sh
+++ dockerimages/build-app/build-all.sh
@@ -6,13 +6,11 @@ set -e
 echo "Cloning"
 git clone https://github.com/kruegerrobotics/lan-monitor.git
 
-echo "changing branch"
 cd lan-monitor
-git checkout ux-upgrade
 
 echo "building the web angular part"
 cd webui-ang
-npm install
+npm install --silent
 
 echo "building the go part"
 cd ..
